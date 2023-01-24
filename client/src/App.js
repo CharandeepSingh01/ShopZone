4df68bf544517e6d340755187ff15eb460da80d7
CharandeepSingh01 <login.charandeep@gmail.com>
2023-01-24 18:23:55 +0530

    changes

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+
+function App() {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("/posts");
+      setPosts(data);
+    };
+    fetchData();
+}, []);
+return (
+
+<>
+<Navbar />
+<Main />
+</>
+)
+};
+
+export default App;
+
+
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const { data } = await axios.get("/posts");
+//       setPosts(data);
+//     };
+
+//     fetchData();
+//   }, []);
+//   return <div>HELLO:{posts && JSON.stringify(posts)}</div>;
+// }
+
+// export default App;
+/* <div>
+  Hello:{posts && JSON.stringify(posts)}
+</div>; */
